80b46441f90dd4da6251a6d42effc447274f56e0
DanMadar27 <danmadar27@gmail.com>
2023-10-18 15:17:07 +0300

    Create telegram bot

--- main.py
+++ main.py
@@ -1,27 +1,8 @@
 from os import makedirs
-from src.services.scraper import get_updates, get_notes
-from src.utils.files import modification_time, html_to_pdf
-from src.config.files import OUTPUT_PATH, WEAPONS_PATH
+from src.services.telegram_bot import start
+from src.config.files import OUTPUT_PATH
 
 if __name__ == '__main__':
     print('Starting...')
-
-    # Ensure the output directory exists, and if not, create it
-    makedirs(OUTPUT_PATH, exist_ok = True)
-
-    # Get updates later than the last fetch
-    updates = get_updates(modification_time(WEAPONS_PATH))
-
-    # If there are latest patch notes, get the notes and convert to pdf
-    if updates:
-        try:
-            update_notes = get_notes(updates[0])
-            html_to_pdf(str(update_notes), WEAPONS_PATH)
-            print('New updates found')
-        except Exception as e:
-            print('Error getting updates: ' + str(e))
-
-    else:
-        print('No new updates')
-
-    print('Finished successfully')
+    makedirs(OUTPUT_PATH, exist_ok = True) # Ensure the output directory exists, and if not, create it
+    start()
---
+++ src/services/telegram_bot.py
@@ -0,0 +1,53 @@
+from os import environ
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+from logging import basicConfig as logging_basicConfig, INFO
+
+from src.services.scraper import get_updates, get_notes
+from src.utils.files import modification_time, html_to_pdf
+from src.config.files import WEAPONS_PATH
+
+logging_basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=INFO
+)
+
+async def start_handler(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await update.message.reply_text(
+        'Hello, I am the Call of Duty Bot. I will keep you updated with the latest patch notes.\n' +
+        'Use /help to see the possible commands.'
+    )
+
+async def help_handler(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await update.message.reply_text(
+        '/help - Show possible commands\n' + 
+        '/updates - Get the latest patch notes of Call of Duty'
+    )
+
+async def updates_handler(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    # Get updates later than the last fetch
+    updates = get_updates(modification_time(WEAPONS_PATH))
+
+    # If there are latest patch notes, get the notes and convert to pdf
+    if updates:
+        try:
+            update_notes = get_notes(updates[0])
+            html_to_pdf(str(update_notes), WEAPONS_PATH)
+            print('New updates found')
+        except Exception as e:
+            print('Error getting updates: ' + str(e))
+
+    else:
+        print('No new updates')
+
+    await context.bot.send_document(chat_id = update.message.chat_id, document = open(WEAPONS_PATH, 'rb'))
+
+def start():
+    print('Starting bot...')
+    app = ApplicationBuilder().token(environ.get('BOT_TOKEN')).build()
+
+    app.add_handler(CommandHandler('start', start_handler))
+    app.add_handler(CommandHandler('help', help_handler))
+    app.add_handler(CommandHandler('updates', updates_handler))
+
+    app.run_polling()
