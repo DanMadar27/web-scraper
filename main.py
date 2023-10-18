79c8e645e04a8bd9cbc8da98088ddd41a1b82dad
DanMadar27 <danmadar27@gmail.com>
2023-10-18 23:52:10 +0300

    Install python-dotenv package

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from os import makedirs
+from dotenv import load_dotenv
 from src.services.telegram_bot import start
 from src.config.files import OUTPUT_PATH
 
 if __name__ == '__main__':
     print('Starting...')
+    load_dotenv()  # take environment variables from .env.
     makedirs(OUTPUT_PATH, exist_ok = True) # Ensure the output directory exists, and if not, create it
     start()
