fbf92661900986fbd6f4fb9ee759500d08bed755
Dan <danmadar27@gmail.com>
2023-11-06 00:45:26 +0200

    Move headers function to scraper utils

--- src/utils/scraper.py
+++ src/utils/scraper.py
@@ -1,3 +1,23 @@
+import random 
+
+# Headers for the scraper requests
+def headers():
+    return {
+        'User-Agent': random_user_agent(),
+    }
+    
+def random_user_agent():
+    user_agents = [ 
+    	'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36', 
+	    'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36', 
+    	'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36', 
+	    'Mozilla/5.0 (iPhone; CPU iPhone OS 12_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148', 
+    	'Mozilla/5.0 (Linux; Android 11; SM-G960U) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.72 Mobile Safari/537.36' 
+    ] 
+
+    return random.choice(user_agents) 
+
+        
 def find_weapons_title(tag):
     return tag.name == 'h3' and tag.get_text().lower() == 'weapons'
 
